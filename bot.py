7a206cdf5c4579bd614997d04a553ddca64aa636
Jaime Barea <barea1396@gmail.com>
2020-02-18 16:37:58 +0100

    Added settings.py

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
+from settings import *
 from telegram.ext import Updater
 
-updater = Updater(token='', use_context=True)
+# Función principal del bot
+def main():
+    updater = Updater(token=TOKEN_BOT, use_context=True)
+
+
+# Ejecución del bot
+if __name__ == '__main__':
+    main()
